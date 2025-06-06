d9c28fadd686989a9f68520d6cd2f4768d97b8ea
Praveen Bharat Jangre <praveen.jangre@impelsys.com>
2025-06-06 20:25:53 +0530

    Initial Project setup

---
+++ CDP/CDP/CDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDPApp.swift
+//  CDP
+//
+//  Created by Praveen Jangre on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
